72e8aeb71408208c0779466e38b1ee0af54c2e46
Ahmet Kalem <ahmet.kalem@kafein.com.tr>
2024-11-10 23:22:31 +0300

    Ocelot yapılandırması tamamlandı

--- CareerNetApiGateway/Api.Gateway/Program.cs
+++ CareerNetApiGateway/Api.Gateway/Program.cs
@@ -3,29 +3,23 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot();
 
 builder.Configuration.AddJsonFile("ocelot.json");
-builder.Services.AddOcelot();
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseOcelot();
-
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
